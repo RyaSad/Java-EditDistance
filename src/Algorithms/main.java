3db082bc1cd75d9b61a8e793b5e91c4cb3471ba7
Ryan <ryansadofsky@gmail.com>
2018-10-02 16:20:07 -0400

    showcasing functionality

--- src/Algorithms/main.java
+++ src/Algorithms/main.java
@@ -17,6 +17,15 @@ public class main {
 		String[] word_list = ed.splitFileContents("PopularWords.txt");
 		
 		ArrayList<MatchObj> matches = ed.findBestMatch(input, word_list);
+		
+		if(matches.size() == 1) {
+			System.out.println("Best match with edit distance " + matches.get(0).getEditDistance() + ":\n" + matches.get(0).getBestMatch());
+		}else {
+			System.out.println("Found " + matches.size() + " possible matches with edit distance "+matches.get(0).getEditDistance() +" :");
+			for(int i = 0; i < matches.size(); i++) {
+				System.out.println(i+1 + ") " + matches.get(i).getBestMatch());
+			}
+		}
 	}
 
 }
